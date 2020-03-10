d308b0842ff13c4bdee475beb5c66e18f16a5111
Stalin llue <61747530+Stalinllue@users.noreply.github.com>
2020-03-10 11:08:30 -0500

    tipos de datos

---
+++ LLUE TIPOS DE DATOS/src/llue/tipos/de/datos/LLUETIPOSDEDATOS.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package llue.tipos.de.datos;
+
+/**
+ *
+ * @author ITQ
+ */
+public class LLUETIPOSDEDATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    //JAJAJJJA DE UNA LINEA//
+    
+    /*
+    EL DIA D EHOT ME LUCY JAJJJAJAJJA
+    PERO CUANDO PUEDA JAAJJA 
+    */
+    int NUMEROENTERO = 5;
+    double numerodouble = 0.0;
+    char letras = 'L';
+    String cadena = "hola mundo";
+    boolean valorbool = false;
+    valorbool = true; 
+    boolean valorboola = false;
+}
